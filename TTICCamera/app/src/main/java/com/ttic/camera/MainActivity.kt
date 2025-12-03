6e8785c1f8f6d71334981a58b6d47275e1ec8b40
momo7077 <19913211869@163.com>
2025-12-03 18:22:04 +0000

    demo2

--- TTICCamera/app/src/main/java/com/ttic/camera/MainActivity.kt
+++ TTICCamera/app/src/main/java/com/ttic/camera/MainActivity.kt
@@ -3,19 +3,22 @@ package com.ttic.camera
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.ttic.camera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
         setSupportActionBar(binding.topAppBar)
         setupActionBarWithNavController(navController)
     }
